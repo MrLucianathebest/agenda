c999216638e69d6d535be659716a57f5a489ffdf
vital-edu <vitaldu@gmail.com>
2013-09-19 22:41:43 -0300

    Criando classe Agenda

---
+++ Agenda.java
@@ -0,0 +1,22 @@
+
+package agenda;
+import java.util.Vector;
+
+/**
+ *
+ * @author Wilton e Eduardo
+ */
+public class Agenda {
+    
+    private Vector listaContatos;
+    
+    public Agenda(){
+        listaContatos = new Vector();
+    }
+    
+    public String adicionarContato(Contato novoContato){
+        listaContatos.add(novoContato);
+        return "Contato Cadastrado com Sucesso!";  
+    }
+    
+}
