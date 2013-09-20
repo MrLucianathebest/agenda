f2870c2f990a471395ec3c9d373e13fc131b7501
vital-edu <vitaldu@gmail.com>
2013-09-19 22:39:52 -0300

    Refazendo a classe Contato

--- Contato.java
+++ Contato.java
@@ -1,87 +1,17 @@
+
+package agenda;
+
+/**
+ *
+ * @author Wilton e Eduardo
+ */
 public class Contato {
-
-    // Atributos
+    
     private String nome;
-    private String sobrenome;
     private String telefone;
-    private String email;
-    private String instantMsg;
-    private String empresa;
-    private String cargo;
-    private String aniversario;
-    private String site;
-    private String grupo;
-    private Object foto;
-
-    //Construtores    
-    public Contato() {
-        this.nome = "-";
-    }
-
-    public Contato(String nome) {
-        this.nome = nome;
-    }
-
-    public Contato(String nome, String telefone) {
-        this.nome = nome;
-        this.telefone = telefone;
-    }
-
-    // Métodos
-    public String getAniversario() {
-        return aniversario;
-    }
-
-    public void setAniversario(String aniversario) {
-        this.aniversario = aniversario;
-    }
-
-    public String getCargo() {
-        return cargo;
-    }
-
-    public void setCargo(String cargo) {
-        this.cargo = cargo;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getEmpresa() {
-        return empresa;
-    }
-
-    public void setEmpresa(String empresa) {
-        this.empresa = empresa;
-    }
-
-    public Object getFoto() {
-        return foto;
-    }
-
-    public void setFoto(Object foto) {
-        this.foto = foto;
-    }
-
-    public String getGrupo() {
-        return grupo;
-    }
-
-    public void setGrupo(String grupo) {
-        this.grupo = grupo;
-    }
-
-    public String getInstantMsg() {
-        return instantMsg;
-    }
-
-    public void setInstantMsg(String instantMsg) {
-        this.instantMsg = instantMsg;
+    
+    public Contato(String nome){
+        this.setNome(nome);
     }
 
     public String getNome() {
@@ -92,22 +22,6 @@ public class Contato {
         this.nome = nome;
     }
 
-    public String getSite() {
-        return site;
-    }
-
-    public void setSite(String site) {
-        this.site = site;
-    }
-
-    public String getSobrenome() {
-        return sobrenome;
-    }
-
-    public void setSobrenome(String sobrenome) {
-        this.sobrenome = sobrenome;
-    }
-
     public String getTelefone() {
         return telefone;
     }
@@ -115,5 +29,5 @@ public class Contato {
     public void setTelefone(String telefone) {
         this.telefone = telefone;
     }
+    
 }
-
