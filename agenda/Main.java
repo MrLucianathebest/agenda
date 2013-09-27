b2c0da37aab39f6b70630cbf789a44a5d5bd97e9
Eduardo Vital <vitaldu@gmail.com>
2013-09-26 21:52:29 -0300

    Refatorando clase: modificandas referências de Contato para Pessoa

---
+++ agenda/Main.java
@@ -0,0 +1,11 @@
+package agenda;
+
+/**
+ *
+ * @author Wilton e Eduardo
+ */
+public class Main {
+    public static void main(String[] args) {
+        new PessoaJFrame().setVisible(true);
+    }
+}
