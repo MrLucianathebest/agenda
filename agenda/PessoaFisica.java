ca469c7eb933c746b2b69ca45878c1854ef46614
Eduardo Vital <vitaldu@gmail.com>
2013-09-26 22:32:28 -0300

    Alterando métodos construtores para modificar diretamente os atributos herdados da classe pai. Também foi excluído os métodos setteres e getteres que já existiam na classe pai.

--- agenda/PessoaFisica.java
+++ agenda/PessoaFisica.java
@@ -4,29 +4,24 @@ package agenda;
 public class PessoaFisica extends Pessoa{
 
     // Atributos
-    private String nome;
     private String sobrenome;
     private String cpf;
-    private String telefone;
-    private String endereco;
-    private String email;
     private String empresa;
     private String cargo;
     private String aniversario;
-    private String site;
     private Object foto;
 
     //Construtores    
     public PessoaFisica(String nome, String cpf) {
-        this.nome = nome;
+        super.nome = nome;
         this.cpf = cpf;
         
     }
 
     public PessoaFisica(String nome,String cpf, String telefone) {
-        this.nome = nome;
+    	super.nome = nome;
+        super.telefone = telefone;
         this.cpf = cpf;
-        this.telefone = telefone;
     }
 
     // Métodos
@@ -46,14 +41,6 @@ public class PessoaFisica extends Pessoa{
         this.cargo = cargo;
     }
 
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
     public String getEmpresa() {
         return empresa;
     }
@@ -70,36 +57,12 @@ public class PessoaFisica extends Pessoa{
         this.foto = foto;
     }
     
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getSite() {
-        return site;
-    }
-
-    public void setSite(String site) {
-        this.site = site;
-    }
-
     public String getSobrenome() {
         return sobrenome;
     }
 
     public void setSobrenome(String sobrenome) {
         this.sobrenome = sobrenome;
-    }
-
-    public String getTelefone() {
-        return telefone;
-    }
-
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
     }
 
 	public String getCpf() {
@@ -110,11 +73,4 @@ public class PessoaFisica extends Pessoa{
 		this.cpf = cpf;
 	}
 
-	public String getEndereco() {
-		return endereco;
-	}
-
-	public void setEndereco(String endereco) {
-		this.endereco = endereco;
-	}
 }
