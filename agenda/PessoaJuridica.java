c22bcbb4ba172ab00c73aab72ec720244c851540
Eduardo Vital <vitaldu@gmail.com>
2013-09-26 22:35:36 -0300

    Alterando métodos construtores para modificar diretamente os atributos herdados da classe pai. Também foi excluído os métodos setteres e getteres que já existiam na classe pai.

--- agenda/PessoaJuridica.java
+++ agenda/PessoaJuridica.java
@@ -4,75 +4,30 @@ package agenda;
 public class PessoaJuridica extends Pessoa{
 
     // Atributos
-    private String nome;
     private String razaoSocial;
     private String cnpj;
-    private String telefone;
-    private String endereco;
-    private String email;
-    private String site;
     
     //Construtores    
     public PessoaJuridica() {
-        this.nome = "-";
+        super.nome = "-";
     }
 
     public PessoaJuridica(String nome) {
-        this.nome = nome;
+        super.nome = nome;
     }
 
     public PessoaJuridica(String nome, String telefone) {
-        this.nome = nome;
-        this.telefone = telefone;
+        super.nome = nome;
+        super.telefone = telefone;
     }
 
     // Métodos
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-    
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getSite() {
-        return site;
-    }
-
-    public void setSite(String site) {
-        this.site = site;
-    }
-
-    public String getTelefone() {
-        return telefone;
-    }
-
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
-
 	public String getCnpj() {
 		return cnpj;
 	}
 
 	public void setCnpj(String cnpj) {
 		this.cnpj = cnpj;
-	}
-
-	public String getEndereco() {
-		return endereco;
-	}
-
-	public void setEndereco(String endereco) {
-		this.endereco = endereco;
 	}
 
 	public String getRazaoSocial() {
